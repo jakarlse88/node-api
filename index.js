480b73bacfb753ff18fc23d4e15628b86e1b89d4
Jon Karlsen <karlsen.jonarild@gmail.com>
2018-07-15 12:43:07 +0200

    Initial scaffolding

---
+++ index.js
@@ -0,0 +1,30 @@
+/*
+ * Primary file for the API. 
+ * 
+ */
+
+ /* Dependencies */
+const http = require('http');
+const url = require('url');
+
+// Server responds to all requests with a string
+const server = http.createServer((req, res) => {
+    
+    // Get URL and parse it
+    const parsedURL = url.parse(req.url, true);
+    
+    // Get path from URL
+    const path = parsedURL.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    
+    // Send response
+    res.end(`Hello, world!\n`);
+
+    // Log request path
+    console.log(`Request received on path: ${trimmedPath}`);
+});
+
+// Start the server and have it listen on port 3000
+server.listen(3000, () => {
+    console.log(`Server listening on port 3000`);
+});
